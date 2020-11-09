e44f6685dac61fb657bddaf43942a1e49607b7ce
Valentin V / vvval <valentin@spiralscout.com>
2020-11-09 12:48:15 +0300

    fix card template

--- views/dashboard/value-card.dark.php
+++ views/dashboard/value-card.dark.php
@@ -3,7 +3,7 @@
     <div class="sf-statpanel">
       <div class="sf-statpanel__text">
         <span class="sf-statpanel__value">${value}</span>
-        <span class="sf-statpanel__label">&nbsp${label}&nbsp;</span>
+        <span class="sf-statpanel__label">&nbsp;${label}&nbsp;</span>
       </div>
       <span class="sf-statpanel__icon">
         <i class="fa fa-${icon}"></i>

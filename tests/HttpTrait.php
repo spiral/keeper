ec95bd3312840db4da3e232efb025dbe4f5bd86c
Valentin V / vvval <valentin@spiralscout.com>
2020-11-17 19:01:10 +0300

    wrap route exeption into 404

--- tests/HttpTrait.php
+++ tests/HttpTrait.php
@@ -7,19 +7,28 @@ namespace Spiral\Tests\Keeper;
 use Laminas\Diactoros\ServerRequest;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Spiral\Router\Exception\RouteNotFoundException;
 
 trait HttpTrait
 {
     private function post($uri, array $data = [], array $headers = [], array $cookies = []): ResponseInterface
     {
-        return $this->app->getHttp()->handle(
-            $this->request($uri, 'POST', [], $headers, $cookies)->withParsedBody($data)
-        );
+        try {
+            return $this->app->getHttp()->handle(
+                $this->request($uri, 'POST', [], $headers, $cookies)->withParsedBody($data)
+            );
+        } catch (RouteNotFoundException $e) {
+            return new NotFoundResponse();
+        }
     }
 
     private function get($uri, array $query = [], array $headers = [], array $cookies = []): ResponseInterface
     {
-        return $this->app->getHttp()->handle($this->request($uri, 'GET', $query, $headers, $cookies));
+        try {
+            return $this->app->getHttp()->handle($this->request($uri, 'GET', $query, $headers, $cookies));
+        } catch (RouteNotFoundException $e) {
+            return new NotFoundResponse();
+        }
     }
 
     private function request(

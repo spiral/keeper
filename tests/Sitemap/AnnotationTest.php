1fc75f1937979c90e7a887ddb1d1555aa3001ec0
Valentin V / vvval <valentin@spiralscout.com>
2020-11-24 19:02:55 +0300

    add sitemap test

--- tests/Sitemap/AnnotationTest.php
+++ tests/Sitemap/AnnotationTest.php
@@ -13,8 +13,69 @@ class AnnotationTest extends TestCase
 
     public function testRoot(): void
     {
-        $response = $this->get('/default/root/self');
-        dump((string)$response->getStatusCode());
-        dump((string)$response->getBody());
+        $output = $this->getSitemap();
+        $this->assertArrayHasKey('simple.method', $output);
+    }
+
+    public function testGroup(): void
+    {
+        $output = $this->getSitemap();
+        $this->assertArrayHasKey('rootgroup', $output);
+    }
+
+    public function testCustomDeclaring(): void
+    {
+        $output = $this->getSitemap();
+        $this->assertArrayHasKey('custom', $output);
+        $this->assertArrayHasKey('custom.parent', $this->nodes($output, 'custom'));
+    }
+
+    public function testLocalParent(): void
+    {
+        $output = $this->getSitemap();
+        $this->assertArrayHasKey('rootgroup', $output);
+        $this->assertArrayHasKey('root.top', $this->nodes($output, 'rootgroup'));
+        $this->assertArrayHasKey('root.bottom', $this->nodes($output, 'rootgroup', 'root.top'));
+    }
+
+    public function testExternalParent(): void
+    {
+        $output = $this->getSitemap();
+
+        //annotation -> custom sitemap
+        $this->assertArrayHasKey('custom', $output);
+        $this->assertArrayHasKey('custom.parent', $this->nodes($output, 'custom'));
+        $this->assertArrayHasKey('root.parent', $this->nodes($output, 'custom', 'custom.parent'));
+
+        //annotation -> annotation
+        $this->assertArrayHasKey('root.child', $this->nodes($output, 'custom', 'custom.parent', 'root.parent'));
+        $this->assertArrayHasKey(
+            'external.index',
+            $this->nodes($output, 'custom', 'custom.parent', 'root.parent', 'root.child')
+        );
+    }
+
+    public function testSiblings(): void
+    {
+        $output = $this->getSitemap();
+        $this->assertArrayHasKey('custom', $output);
+        $this->assertArrayHasKey('custom.parent', $this->nodes($output, 'custom'));
+        $this->assertArrayHasKey('root.parent', $this->nodes($output, 'custom', 'custom.parent'));
+        $this->assertArrayHasKey('external.custom', $this->nodes($output, 'custom', 'custom.parent'));
+    }
+
+    private function getSitemap(): array
+    {
+        $response = $this->get('/default/sitemap');
+        return json_decode((string)$response->getBody(), true);
+    }
+
+    private function nodes(array $sitemap, string $firstKey, string ...$keys): array
+    {
+        array_unshift($keys, $firstKey);
+        foreach ($keys as $key) {
+            $sitemap = $sitemap[$key]['nodes'];
+        }
+        return $sitemap;
     }
 }

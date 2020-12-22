e439b4f7761f2d300879b4801224a83f766ba53e
Valentin V / vvval <valentin@spiralscout.com>
2020-12-22 18:55:23 +0300

    add conditions in tabs template

--- views/layout/tabs/tab.dark.php
+++ views/layout/tabs/tab.dark.php
@@ -1,28 +1,32 @@
 <stack:push name="tab-headers">
-  @if(!injected('permission') || $_guard_->allows(inject('permission'), inject('permission-context', [])))
-    <li class="nav-item">
-      <a data-sf="tabnav-tab"
-         class="nav-link@if(inject('active') === true) active@endif"
-         id="${id}-tab"
-         href="#${id}"
-         role="tab"
-         aria-controls="${id}"
-         aria-selected="${active}">
-        @if(injected('icon'))<span class="fa fa-${icon}"></span> @endif${title}
-      </a>
-    </li>
-  @endif
+    @if(!injected('condition') || inject('condition'))
+        @if(!injected('permission') || $_guard_->allows(inject('permission'), inject('permission-context', [])))
+            <li class="nav-item">
+                <a data-sf="tabnav-tab"
+                   class="nav-link@if(inject('active') === true) active@endif"
+                   id="${id}-tab"
+                   href="#${id}"
+                   role="tab"
+                   aria-controls="${id}"
+                   aria-selected="${active}">
+                    @if(injected('icon'))<span class="fa fa-${icon}"></span> @endif${title}
+                </a>
+            </li>
+        @endif
+    @endif
 </stack:push>
 
 <stack:push name="tab-body">
-  @if(!injected('permission') || $_guard_->allows(inject('permission'), inject('permission-context', [])))
-    <div class="sf-tabnav__content tab-pane@if(inject('active') === true) active@endif"
-         id="${id}"
-         role="tabpanel"
-         aria-labelledby="${id}-tab">
-      <div class="sf-main__wrapper">
-        ${context}
-      </div>
-    </div>
-  @endif
+    @if(!injected('condition') || inject('condition'))
+        @if(!injected('permission') || $_guard_->allows(inject('permission'), inject('permission-context', [])))
+            <div class="sf-tabnav__content tab-pane@if(inject('active') === true) active@endif"
+                 id="${id}"
+                 role="tabpanel"
+                 aria-labelledby="${id}-tab">
+                <div class="sf-main__wrapper">
+                    ${context}
+                </div>
+            </div>
+        @endif
+    @endif
 </stack:push>
